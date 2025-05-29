3637a68f9b7250a91055cc5c49547f93b552aa15
suganth3 <suganthkrishna3@gmail.com>
2025-05-29 07:07:28 +0000

    building spring user

--- springappuser/src/main/java/com/examly/springappuser/config/JwtAuthenticationFilter.java
+++ springappuser/src/main/java/com/examly/springappuser/config/JwtAuthenticationFilter.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import io.jsonwebtoken.*;
 
 import java.io.IOException;
--- springappuser/src/main/java/com/examly/springappuser/model/LoginDTO.java
+++ springappuser/src/main/java/com/examly/springappuser/model/LoginDTO.java
@@ -1,10 +1,13 @@
 package com.examly.springappuser.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class LoginDTO {
     private String token;
     private String username;
